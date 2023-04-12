0fe64006582d224a4d2e6886261ce5f27652a05e
itaytzdaka <itaytzdaka1@gmail.com>
2023-04-12 17:52:12 +0300

    iframes

--- main.js
+++ main.js
@@ -5,45 +5,51 @@
             name: "teamWork",
             src1: "./assets/img/TEAMWORK LOGO.svg",
             src2: "./assets/img/TEAMWORK LOGO.svg",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/teamwork.html",
         },
         {
             name: "MultiNet",
             src1: "./assets/img/cropped-logo-white-01.png",
             src2: "./assets/img/cropped-logo-blue-01.png",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/multiNet.html",
         },
         {
             name: "PURPLELENS",
             src1: "./assets/img/purplelens.png",
             src2: "./assets/img/purplelens.png",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/purplelens.html",
         },
         {
             name: "Meat2me",
             src1: "./assets/img/LOGO MEAT PNG.png",
             src2: "./assets/img/LOGO MEAT PNG.png",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/meat2me.html",
         },
         {
             name: "CONTACT",
             src1: "./assets/img/unnamed.png",
             src2: "./assets/img/unnamed.png",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/contact.html",
         },
         {
             name: "PILATES",
             src1: "./assets/img/pilates logo.png",
             src2: "./assets/img/pilates logo.png",
-            span: "מהיום הכל במקום אחד"
+            span: "מהיום הכל במקום אחד",
+            html: "./iframes/pilates.html",
         },
-
     ];
 
     const ul = document.querySelector("ul");
+    const main = document.querySelector("main");
 
 
-    // add list to ul in the html and handle onclick
+    // For any logo add li to ul and iframe and handle onclick.
     logoArray.forEach((logo, index) => {
 
 
@@ -55,16 +61,25 @@
         </div>
         `;
 
+        const iframe = document.createElement("iframe");
+        iframe.src = logoArray[index].html;
+
         li.addEventListener("click", (e) => {
 
-            //remove .active
+            //remove active
             const liArray = document.querySelectorAll("li");
+            const iframeArray = document.querySelectorAll("iframe");
+
             liArray.forEach(li => {
-                li.classList.remove("active");
+                li.classList.remove("li-active");
+            })
+            iframeArray.forEach(li => {
+                li.classList.remove("iframe-active");
             })
 
             //add .active
-            e.currentTarget.classList.add("active");
+            e.currentTarget.classList.add("li-active");
+            iframe.classList.add("iframe-active");
 
             
             //change src to default
@@ -74,13 +89,16 @@
             });
 
 
-            //change active img src
+            //change li-active img src
             const img = e.currentTarget.querySelector("img");
             img.src = logoArray[index].src2;
 
         });
 
+        //add elements to html
+        
         ul.appendChild(li);
+        main.appendChild(iframe);
     });
 
 })();
